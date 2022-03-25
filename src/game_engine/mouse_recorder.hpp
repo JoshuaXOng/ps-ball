3969053865ea9c476d079f0ceb7130db993e36ab
JoshuaXOng <joshuaong2000@gmail.com>
2022-03-25 13:12:11 +1100

    Updated mouse recorder.

--- src/game_engine/mouse_recorder.hpp
+++ src/game_engine/mouse_recorder.hpp
@@ -5,84 +5,63 @@
 #include <utility>
 #include <iostream>
 #include <assert.h>
+#include <stdio.h>
 #include <exception>
-#include "../utils/physics_utils.hpp"
-#include "../utils/time_utils.hpp"
+#include <deque>
+#include "time_utils.hpp"
+#include "physics_utils.hpp"
+#include "types.hpp"
 
 class MouseRecorder {
 	public:
-		MouseRecorder(int pollingInterval, int storageSize) {
-			if (storageSize <= 2) 
-				throw std::runtime_error("MouseRecorder object should be init'd with more that 2 storage size.");
-			if (pollingInterval < 100)
-				throw std::runtime_error("Polling interval cannot be lower than 100ms.");
-
-			this->pollingInterval = pollingInterval;
+		MouseRecorder() {
+			for (int i = 0; i < this->capacity; i++)
+				this->updatePrevPositions();
 			TimeUtils::setIntervalThreaded(this->pollingInterval, [=]() {
-				this->recordPosition();
+				this->updatePrevPositions();
 			});
-
-			SDL_Point currentPosition; 
-			SDL_GetMouseState(&currentPosition.x, &currentPosition.y);
-			for (int i = 0; i < storageSize; i++)
-				this->previousPositions.push_back(currentPosition);
-
-			// assert(this->previousPositions.size() == storageSize);
 		};
 		~MouseRecorder() {};
-
-		void printPreviousPositions() {
-			std::cout << "========== Mouse Positions ==========" << std::endl;   
-			for (SDL_Point point : MouseRecorder::previousPositions)
-				std::cout << "x position: " << point.x << ", y position: " << point.y << std::endl;
-			std::cout << "========== End Mouse Positions ==========" << std::endl;   
-		};
 
 		SDL_Point getCurrentPosition() {
 			SDL_Point position; 
 			SDL_GetMouseState(&position.x, &position.y);
-			return { position.x, position.y };
+			return position;
 		};
 
-		std::pair<float, float> getCurrentVelocityComponents() {
-			return this->getVelocityComponents(0);
+		void printPreviousPositions(int num) {
+			std::deque<SDL_Point>::iterator it = this->previousPositions.end();
+			for (int i = 0; i < num; i++) {
+				SDL_Point point = *it--;
+				printf("Coordinates of mouse: %i, %i", point.x, point.y);
+			}
+		};
+
+		TwoDVector getCurrentVelocity() {
+			return this->getVelocity(0);
 		};
 
 		float getCurrentSpeed() {
 			return this->getSpeed(0);
 		};
 
-		double getCurrentAcceleration() {
-			float newestSpeed = this->getCurrentSpeed();
-			float laterSpeed = this->getSpeed(1);
-			return PhysicsUtils::calcAcceleration(laterSpeed, newestSpeed, this->pollingInterval);
-		};
-
-		std::pair<float, float> getCurrentAccelerationComponents() {
-			std::pair<float, float> currentVelocityComponents = this->getCurrentVelocityComponents();
-    	std::pair<float, float> previousVelocityComponents = this->getVelocityComponents(1);
-
-			float xAcceleration = (currentVelocityComponents.first - previousVelocityComponents.first) / this->pollingInterval;
-			float yAcceleration = (currentVelocityComponents.second - previousVelocityComponents.second) / this->pollingInterval;
-
-			return std::make_pair(xAcceleration, yAcceleration);
+		std::pair<float, float> getCurrentAcceleration() {
+			return this->getAcceleration(0);
 		};
 
 	private:
-		int pollingInterval;
+		int pollingInterval = 50;
+		int capacity = 100;
+		std::deque<SDL_Point> previousPositions = {};
 
-		std::vector<SDL_Point> previousPositions = { };
-
-		float getSpeed(int offset) {
-			SDL_Point mostRecentPosition = this->previousPositions.end()[-1 - offset];
-			SDL_Point secondMostRecentPosition = this->previousPositions.end()[-2 - offset];
-
-			float distance = PhysicsUtils::calcDistance(mostRecentPosition, secondMostRecentPosition);
-			float speed = PhysicsUtils::calcSpeed(distance, this->pollingInterval);
-
-			return speed;
+		void updatePrevPositions() {
+			SDL_Point position = MouseRecorder::getCurrentPosition();
+			MouseRecorder::previousPositions.push_back(position);
+			if (this->previousPositions.size() > this->capacity)
+				MouseRecorder::previousPositions.pop_front();
 		};
-		std::pair<float, float> getVelocityComponents(int offset) {
+
+		std::pair<float, float> getVelocity(int offset) {
 			SDL_Point currentPosition = this->previousPositions.end()[-1 - offset];
 			SDL_Point previousPosition = this->previousPositions.end()[-2 - offset];
 
@@ -94,9 +73,23 @@ class MouseRecorder {
 			return std::make_pair(xVelocity, yVelocity);
 		};
 
-		void recordPosition() {
-			SDL_Point position = MouseRecorder::getCurrentPosition();
-			MouseRecorder::previousPositions.push_back(position);
-			MouseRecorder::previousPositions.erase(MouseRecorder::previousPositions.begin());
+		float getSpeed(int offset) {
+			SDL_Point currentPosition = this->previousPositions.end()[-1 - offset];
+			SDL_Point previousPosition = this->previousPositions.end()[-2 - offset];
+
+			float distance = PhysicsUtils::calcDistance(currentPosition, previousPosition);
+			float speed = PhysicsUtils::calcSpeed(distance, this->pollingInterval);
+
+			return speed;
+		};
+
+		std::pair<float, float> getAcceleration(int offset) {
+			std::pair<float, float> currentVelocity = this->getVelocity(0 + offset);
+			std::pair<float, float> previousVelocity = this->getVelocity(1 + offset);
+
+			float xAcceleration = (currentVelocity.first - previousVelocity.first) / this->pollingInterval;
+			float yAcceleration = (currentVelocity.second - previousVelocity.second) / this->pollingInterval;
+
+			return std::make_pair(xAcceleration, yAcceleration);
 		};
 };
